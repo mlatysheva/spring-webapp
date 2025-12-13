621aea04990f9eb75dafdb56ef54250de1549a4f
Maria <mlatysheva@gmail.com>
2025-12-13 19:32:23 +0100

    feat: added toString, hibernate equals() and hashCode() methods

--- src/main/java/springframework/spring_webapp/domain/Book.java
+++ src/main/java/springframework/spring_webapp/domain/Book.java
@@ -2,6 +2,7 @@ package springframework.spring_webapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -47,5 +48,28 @@ public class Book {
 
     public void setIsdn(String isdn) {
         this.isdn = isdn;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isdn='" + isdn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
